d5176dfd3bc1e4af8f718f839a17f2d9b83513f1
Davi Ximenes <davix.ufc@gmail.com>
2019-11-01 00:09:17 -0300

    First

---
+++ app.js
@@ -0,0 +1,12 @@
+var scores, roundScore, activePlayer;
+
+
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice =Math.floor(Math.random()* 7);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
